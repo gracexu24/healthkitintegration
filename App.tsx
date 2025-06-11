3cd3b0f8b7d71ea2ab14f1f817966d891d3ca9c8
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2025-06-11 15:46:10 -0700

    code should work

--- App.tsx
+++ App.tsx
@@ -1,19 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import useHealthData from './src/hooks/useHealthData';
-
+import Value from './src/components/Value';
 
 export default function App() {
-  const {steps, distance, flights} = useHealthData();
+  const [date, setDate] = useState(new Date());
+
+	const changeDate = (numDays) => {
+    const currentDate = new Date(date); // Create a copy of the current date
+    // Update the date by adding/subtracting the number of days
+    currentDate.setDate(currentDate.getDate() + numDays);
+
+    setDate(currentDate); // Update the state variable
+  };
+    setDate(currentDate); 
+  const {steps, distance, flights} = useHealthData(date);
 
 	console.log(`Steps: ${steps} | Distance: ${distance}m | Flights: ${flights}`);
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Testing!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.datePicker}>
+        <AntDesign
+          onPress={() => changeDate(-1)}
+          name="left"
+          size={20}
+          color="#C3FF53"
+        />
+        <Text style={styles.date}>{date.toDateString()}</Text>
+
+        <AntDesign
+          onPress={() => changeDate(1)}
+          name="right"
+          size={20}
+          color="#C3FF53"
+        />
+      </View>
+
+      <View style={styles.values}>
+        <Value label="Steps" value={steps.toString()} />
+        <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`} />
+        <Value label="Flights Climbed" value={flights.toString()} />
+      </View>
+
     </View>
     
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -22,5 +57,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
+    marginTop: 100,
   },
 });
--- src/hooks/useHealthData.ts
+++ src/hooks/useHealthData.ts
@@ -1,11 +1,74 @@
 import { useEffect, useState } from 'react';
+import AppleHealthKit, {
+  HealthInputOptions,
+  HealthKitPermissions,
+  HealthUnit,
+} from "react-native-health";
 
-const useHealthData = () => {
+const { Permissions } = AppleHealthKit.Constants;
+
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      Permissions.Steps,
+      Permissions.FlightsClimbed,
+      Permissions.DistanceWalkingRunning,
+    ],
+    write: [],
+  },
+};
+
+const useHealthData = (date: Date) => {
+  const [hasPermissions, setHasPermission] = useState(false);
   const [steps, setSteps] = useState(0);
   const [flights, setFlights] = useState(0);
   const [distance, setDistance] = useState(0);
 
-	// HealthKit implementation
+  useEffect(() => {
+    AppleHealthKit.initHealthKit(permissions, (err) => {
+      if (err) {
+        console.log('Error getting permissions');
+        return;
+      }
+      setHasPermission(true);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      return;
+    }
+    const options: HealthInputOptions = {
+      date: new Date().toISOString(),
+      includeManuallyAdded: false,
+    };
+    
+    AppleHealthKit.getStepCount(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps');
+        return;
+      }
+      setSteps(results.value);
+    });
+
+    AppleHealthKit.getFlightsClimbed(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps:', err);
+        return;
+      }
+      setFlights(results.value);
+    });
+
+    AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
+      if (err) {
+        console.log('Error getting the steps:', err);
+        return;
+      }
+      setDistance(results.value);
+    });
+  
+    // Query Health data
+  }, [hasPermissions, date]);
 
   return { steps, flights, distance };
 };

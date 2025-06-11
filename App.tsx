f16f1088130169c07762ce1cf3b3ec17f4c1da51
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2025-06-11 12:53:09 -0700

    hook

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import useHealthData from './src/hooks/useHealthData';
+
 
 export default function App() {
+  const {steps, distance, flights} = useHealthData();
+
+	console.log(`Steps: ${steps} | Distance: ${distance}m | Flights: ${flights}`);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
